972a2d2418aab6bf88d10597f74bf6b21e3bce44
ZHQ <herman.zheng@aya.yale.edu>
2021-12-26 23:11:40 -0500

    sticky searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,10 @@ import styled from "styled-components";
 const StyledSearch = styled.input`
   width: 100%;
   margin: 10px 0 20px 0;
-  padding: 0.5rem 1rem;
   outline: none;
+  padding: 0.5rem 1rem;
+  position: sticky;
+  top: 15px;
 `;
 
 export default function Search() {
