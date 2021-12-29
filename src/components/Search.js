a7c59dc51c2dc3873d9de9907f2935ee9296fdbe
ZHQ <herman.zheng@aya.yale.edu>
2021-12-29 17:02:09 -0500

    changed the searchbar's z-index

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledSearch = styled.input`
-  height: 36px;
-  margin: 20px 0;
+  height: 2.5rem;
+  margin: 1.25rem 0;
   outline: none;
   padding: 0.5rem 1rem;
   position: sticky;
-  top: 15px;
+  top: 1rem;
   width: 100%;
+  z-index: 100;
 `;
 
 export default function Search() {
