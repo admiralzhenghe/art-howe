03315ce2eba2d14040e4f1efe1ec8397adf9c74a
ZHQ <herman.zheng@aya.yale.edu>
2022-01-01 21:33:20 -0500

    created an array of image URLs to pass into the ImageGallery component

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // Apollo
 import { useQuery } from "@apollo/client";
 // Components
 import ArtworkDetail from "./ArtworkDetail";
-import ImageGallery from "./ImageGallery";
 import Spinner from "./Spinner";
-// Queries
-import { GET_POST_DETAIL } from "../GraphQL/queries";
+// GraphQL
+import { GET_POST_DETAIL, GET_POST_IMAGES } from "../GraphQL/queries";
+// ImageGallery
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 // Styled
 import styled from "styled-components";
 
@@ -14,9 +16,9 @@ const StyledArtworkContainer = styled.div`
   display: flex;
   grid-row: 3;
   overflow: hidden;
-  animation: animateContainer 0.25s;
+  animation: animateImage 0.25s;
 
-  @keyframes animateContainer {
+  @keyframes animateImage {
     from {
       opacity: 0;
     }
@@ -38,17 +40,58 @@ const StyledImageGalleryContainer = styled.div`
   flex: 1 1 100%;
 `;
 
-export default function Artwork({ artwork }) {
-  const { error, loading, data } = useQuery(GET_POST_DETAIL(artwork));
+export default function Artwork({ postInfo }) {
+  const { id, postTitle } = postInfo;
+  const [loading, setLoading] = useState(true);
+  const [galleryLoading, setGalleryLoading] = useState(true);
+  const [images, setImages] = useState([]);
+  // Query API
+  const { loading: postLoading, data: postData } = useQuery(
+    GET_POST_DETAIL(id)
+  );
+  const { loading: imagesLoading, data: imagesData } = useQuery(
+    GET_POST_IMAGES(postTitle)
+  );
+
+  useEffect(() => {
+    if (!imagesLoading) {
+      // Filter out images with similar titles but do not belong to the same post
+      let filtered = imagesData.mediaItems.edges.filter((image) => {
+        if (image.node.parentId === id) return image.node.sourceUrl;
+      });
+      // Extract image URLs only
+      let extracted = filtered.map((image) => ({
+        original: image.node.sourceUrl,
+      }));
+      setImages(extracted);
+    }
+  }, [imagesLoading]);
+
+  // Once the images array is created set loading to false
+  useEffect(() => {
+    if (!postLoading && images.length) {
+      setLoading(false);
+    }
+  }, [postLoading, images]);
+
+  const handleImageLoad = () => {
+    setGalleryLoading(false);
+  };
 
   if (loading) return <Spinner />;
 
   return (
     <StyledArtworkContainer>
       <StyledImageGalleryContainer>
-        <ImageGallery imageUrl={data.post.featuredImage.node.sourceUrl} />
+        {galleryLoading && <Spinner />}
+        <ImageGallery
+          items={images}
+          onImageLoad={handleImageLoad}
+          showPlayButton={false}
+          showThumbnails={false}
+        />
       </StyledImageGalleryContainer>
-      <ArtworkDetail data={data} />
+      <ArtworkDetail data={postData} />
     </StyledArtworkContainer>
   );
 }
