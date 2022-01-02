c8f831e91a4a5f3f887ad31ffaf7fd6b4d6d117c
ZHQ <herman.zheng@aya.yale.edu>
2022-01-02 18:28:51 -0500

    added new states to handle search status and search terms

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import "./App.css";
 // React
 import { useState } from "react";
-
 // Components
 import Header from "./components/Header";
 import Mosaic from "./components/Mosaic";
 import Search from "./components/Search";
 
-// Router
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 function App() {
+  const [searching, setSearching] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const [viewing, setViewing] = useState(false);
   return (
-    <Router>
-      <div className="App">
-        <Header setViewing={setViewing} />
-        <Search />
-        <Routes>
-          <Route
-            path=""
-            element={<Mosaic viewing={viewing} setViewing={setViewing} />}
-          />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Header
+        setSearching={setSearching}
+        setSearchTerm={setSearchTerm}
+        setViewing={setViewing}
+      />
+      <Search
+        setSearching={setSearching}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        setViewing={setViewing}
+      />
+      <Mosaic
+        searching={searching}
+        setSearching={setSearching}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        viewing={viewing}
+        setViewing={setViewing}
+      />
+    </>
   );
 }
 
