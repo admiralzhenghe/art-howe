b2aabaccd27dd9785ed9051b24ba11ce5eec4680
ZHQ <heqzheng@gmail.com>
2022-02-07 13:14:39 -0500

    fixed bug so that a loading spinner appears when searching for a query

--- src/components/Mosaic.js
+++ src/components/Mosaic.js
@@ -93,7 +93,7 @@ export default function Mosaic() {
     }
   }, [thumbnailLoading, mediumLoading]);
 
-  if (loading || !data) return <Spinner />;
+  if (loading || !thumbnailData || !mediumData) return <Spinner />;
 
   if (viewingArtwork) return <Artwork />;
 
