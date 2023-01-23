9619e6edf4e577071ab17ba74eb622095d93e5cf
hqz3 <heqzheng@gmail.com>
2023-01-23 18:00:44 -0500

    Refactor and remove useEffect hooks

--- src/hooks/useFetchFilters.js
+++ src/hooks/useFetchFilters.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 // Apollo
 import { useQuery } from "@apollo/client";
 // GraphQL
 import { GET_ALL_ARTISTS, GET_ALL_CATEGORIES } from "../GraphQL/queries";
 
 export function useFetchFilters() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   const { loading: artistsLoading, data: artistsData } =
     useQuery(GET_ALL_ARTISTS);
@@ -13,19 +13,16 @@ export function useFetchFilters() {
   const { loading: categoriesLoading, data: categoriesData } =
     useQuery(GET_ALL_CATEGORIES);
 
-  useEffect(() => {
-    if (!artistsLoading && !categoriesLoading) {
-      // Remove duplicate artists using a set
-      const set = new Set();
-      artistsData.posts.nodes.forEach((node) => {
-        set.add(node.details.artist);
-      });
-      // Sort the artists in alphabetical order
-      const filteredArtistsData = [...set].sort();
-      setData([filteredArtistsData, categoriesData]);
-    }
-  }, [artistsLoading, categoriesLoading]);
-
+  if (!artistsLoading && !categoriesLoading && !data) {
+    // Remove duplicate artists using a set
+    const set = new Set();
+    artistsData.posts.nodes.forEach((node) => {
+      set.add(node.details.artist);
+    });
+    // Sort the artists in alphabetical order
+    const filteredArtistsData = [...set].sort();
+    setData([filteredArtistsData, categoriesData]);
+  }
   // return [loading, data]
-  return [data.length ? false : true, data];
+  return data === null ? [true, []] : [false, data];
 }
--- src/hooks/useFetchMosaic.js
+++ src/hooks/useFetchMosaic.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 // Apollo
 import { useQuery } from "@apollo/client";
 // GraphQL
@@ -6,11 +6,10 @@ import { GET_THUMBNAILS } from "../GraphQL/queries";
 // Utils
 import generateMosaicData from "../utils/generateMosaicData";
 
-export function useFetchMosaic() {
-  // Toggle mosaic shuffle
-  const shuffleMosaic = useRef(true);
+const shuffleMosaic = true;
 
-  const [data, setData] = useState([]);
+export function useFetchMosaic() {
+  const [data, setData] = useState(null);
   const { loading: thumbnailLoading, data: thumbnailData } = useQuery(
     GET_THUMBNAILS("THUMBNAIL")
   );
@@ -18,28 +17,26 @@ export function useFetchMosaic() {
     GET_THUMBNAILS("MEDIUM")
   );
 
-  // Create the data and URL needed to generate the images mosaic
-  useEffect(() => {
-    if (!thumbnailLoading && !mediumLoading) {
-      const filteredData = generateMosaicData(thumbnailData, mediumData);
+  if (!thumbnailLoading && !mediumLoading && !data) {
+    // Create the data and URL needed to generate the images mosaic
+    const filteredData = generateMosaicData(thumbnailData, mediumData);
 
-      if (shuffleMosaic.current) {
-        const getRandomInt = (max) => {
-          return Math.floor(Math.random() * max);
-        };
-        const swap = (array, idx1, idx2) => {
-          [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
-        };
-        // Fisher-Yates algorithm to generate a randomly ordered mosaic
-        for (let i = filteredData.length - 1; i >= 0; i--) {
-          let randomIdx = getRandomInt(i);
-          swap(filteredData, i, randomIdx);
-        }
+    if (shuffleMosaic) {
+      const getRandomInt = (max) => {
+        return Math.floor(Math.random() * max);
+      };
+      const swap = (array, idx1, idx2) => {
+        [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+      };
+      // Fisher-Yates algorithm to generate a randomly ordered mosaic
+      for (let i = filteredData.length - 1; i >= 0; i--) {
+        let randomIdx = getRandomInt(i);
+        swap(filteredData, i, randomIdx);
       }
-      setData(filteredData);
     }
-  }, [thumbnailLoading, mediumLoading]);
+    setData(filteredData);
+  }
 
   // return [loading, data]
-  return [data.length ? false : true, data];
+  return data === null ? [true, []] : [false, data];
 }
