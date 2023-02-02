7dfd13b5a0548faff0166649bbd8f6c4a27dfefa
hqz3 <heqzheng@gmail.com>
2023-02-02 16:37:06 -0500

    Simplify mosaic shuffle algorithm

--- src/hooks/useFetchMosaic.js
+++ src/hooks/useFetchMosaic.js
@@ -6,8 +6,6 @@ import { GET_THUMBNAILS } from "../GraphQL/queries";
 // Utils
 import generateMosaicData from "../utils/generateMosaicData";
 
-const shuffleMosaic = true;
-
 export function useFetchMosaic() {
   const [data, setData] = useState(null);
   const { loading: thumbnailLoading, data: thumbnailData } = useQuery(
@@ -20,20 +18,8 @@ export function useFetchMosaic() {
   if (!thumbnailLoading && !mediumLoading && !data) {
     // Create the data and URL needed to generate the images mosaic
     const filteredData = generateMosaicData(thumbnailData, mediumData);
-
-    if (shuffleMosaic) {
-      const getRandomInt = (max) => {
-        return Math.floor(Math.random() * max);
-      };
-      const swap = (array, idx1, idx2) => {
-        [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
-      };
-      // Fisher-Yates algorithm to generate a randomly ordered mosaic
-      for (let i = filteredData.length - 1; i >= 0; i--) {
-        let randomIdx = getRandomInt(i);
-        swap(filteredData, i, randomIdx);
-      }
-    }
+    // Shuffle the data
+    filteredData.sort(() => 0.5 - Math.random());
     setData(filteredData);
   }
 
